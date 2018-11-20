894f73429b6ddc0b0aaf3ba7ccae7f8558cc54f9
Leo.W <adwyxx@qq.com>
2018-11-20 22:41:20 +0800

    init web app

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -3,9 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui' /* 引入全局element-ui */
+import 'element-ui/lib/theme-chalk/index.css' /* 引入全局element-ui样式 */
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ webapp/src/pages/Login.vue
@@ -0,0 +1,17 @@
+<template>
+<div style="width:400px;margin-left:auto;margin-right:auto;magrin-top:200px;">
+    <el-card shadow="hover">
+        <el-form>
+            <el-form-item label="用户名">
+            <el-input type="input" placeholder="请输入用户名"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+            <el-input type="password" placeholder="请输入密码"></el-input>
+            </el-form-item>
+            <el-form-item>
+            <el-button type="primary">登陆</el-button>
+            </el-form-item>
+        </el-form>
+    </el-card>
+</div>
+</template>
